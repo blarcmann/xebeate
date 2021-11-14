9e8fdbf6e0ada58eed8e616359b88217c8afdf57
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2021-11-14 18:55:55 +0100

    desktop sidebar...

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -19,43 +19,32 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar_header border-b border-gray-200 from-gray-100 to-gray-50 bg-gradient-to-t  uk-visible@s">
-        <a href="#">
-          <img src="assets/images/logo.png" />
-          <img src="assets/images/logo-light.png" className="logo_inverse" />
-        </a>
-        {/* <!--btn night mode--> */}
-        <a href="#" id="night-mode" className="btn-night-mode" data-tippy-placement="left" title="Switch to dark mode"></a>
+        <img src={require('../assets/images/logo.png').default} className="sidebar-logo" />
       </div>
-      <div className="border-b border-gray-20 flex justify-between items-center p-3 pl-5 relative uk-hidden@s">
-        <h3 className="text-xl"> Navigation </h3>
-        <span className="btn-mobile" uk-toggle="target: #wrapper ; cls: sidebar-active"></span>
-      </div>
-      <div className="sidebar_inner" data-simplebar>
+      <div className="sidebar_inner">
         <div className="flex flex-col items-center my-6 uk-visible@s">
-          <div
-            className="bg-gradient-to-tr from-yellow-600 to-pink-600 p-1 rounded-full transition m-0.5 mr-2  w-24 h-24">
-            <img src="assets/images/avatars/avatar-2.jpg"
+          <div className="w-32 h-32">
+            <img src={require('../assets/images/anton-krasko-01.jpeg').default}
               className="bg-gray-200 border-4 border-white rounded-full w-full h-full" />
           </div>
-          <a href="profile.html" className="text-xl font-medium capitalize mt-4 uk-link-reset"> Nina Cinderella
-          </a>
+          <span className="text-sm font-medium uppercase mt-2"> Nina Cinderella</span>
           <div className="flex justify-around w-full items-center text-center uk-link-reset text-gray-800 mt-6">
             <div>
+              <div>
+                <span className="text-xs block">Post</span >
+                <span className="font-light text-gray-400"> 130</span>
+              </div>
+            </div>
+            <div>
               <a href="#">
-                <strong>Post</strong>
-                <div> 130</div>
+                <span className="text-xs block">Following</span >
+                <span className="font-light text-gray-400"> 1,230</span>
               </a>
             </div>
             <div>
               <a href="#">
-                <strong>Following</strong>
-                <div> 1,230</div>
-              </a>
-            </div>
-            <div>
-              <a href="#">
-                <strong>Followers</strong>
-                <div> 2,430</div>
+                <span className="text-xs block">Followers</span >
+                <span className="font-light text-gray-400">2,430</span>
               </a>
             </div>
           </div>
